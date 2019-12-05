b81f9c426de11df03d06fabd61865f728187a8a6
stastakh <stastakh@gmail.com>
2019-12-05 21:43:06 +0200

    add query for finding all students with worst score for homework

--- app.js
+++ app.js
@@ -4,15 +4,37 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'trainincenter';
+const dbName = 'Local1';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, { useUnifiedTopology: true}, async function(err, client) {
-    const db = client.db(dbName);
-    const users = db.collection('users');
-    const articles = db.collection('articles');
+MongoClient.connect(url, { useUnifiedTopology: true }, async function(
+  err,
+  client
+) {
+  const db = client.db(dbName);
+  const users = db.collection('users');
+  const articles = db.collection('articles');
+  const students = db.collection('students');
 
-    console.log('works :)');
+  const ratingScore = 50;
 
-    client.close();
+  const printData = arr => {
+    arr.forEach(elem => {
+      console.log(elem);
+    });
+  };
+
+  // Find all students who have the worst score
+  // for homework, sort by descent
+  const worstHomeworkScores = await students
+    .find({
+      'scores.2.type': 'homework',
+      'scores.2.score': { $lt: 50 }
+    })
+    .sort({ 'scores.2.score': -1 })
+    .toArray();
+
+  printData(worstHomeworkScores);
+
+  client.close();
 });
